cd6220095d88c6d47a1dfc9e61068d6d933b5b9d
hrTomal <hr.tomal@gmail.com>
2024-09-27 03:31:52 +0600

    working on login

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { CsNavBarComponent } from "../../shared/cs-nav-bar/cs-nav-bar.component";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    CsNavBarComponent
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
---
+++ src/app/shared/cs-nav-bar/cs-nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cs-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './cs-nav-bar.component.html',
+  styleUrl: './cs-nav-bar.component.css'
+})
+export class CsNavBarComponent {
+
+}
