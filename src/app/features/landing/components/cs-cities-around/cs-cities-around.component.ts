e038239c9d91f4853d2a67261050b9cb753bc60c
hrTomal <hr.tomal@gmail.com>
2024-09-30 01:18:05 +0600

    changes

--- src/app/features/landing/components/cs-cities-around/cs-cities-around.component.ts
+++ src/app/features/landing/components/cs-cities-around/cs-cities-around.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cs-cities-around',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './cs-cities-around.component.html',
   styleUrl: './cs-cities-around.component.css'
 })
 export class CsCitiesAroundComponent {
+  translate : TranslateService = inject(TranslateService); 
 
 }
--- src/app/shared/cs-nav-bar/cs-nav-bar.component.ts
+++ src/app/shared/cs-nav-bar/cs-nav-bar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language/language.service';
 
 @Component({
   selector: 'app-cs-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './cs-nav-bar.component.html',
   styleUrl: './cs-nav-bar.component.css'
 })
 export class CsNavBarComponent {
+  translate: TranslateService = inject(TranslateService);
+  selectedLanguage? : string;
 
+  constructor(private languageService : LanguageService){}
+
+  ngOnInit() {
+    this.languageService.initializeLanguage();
+    this.selectedLanguage = this.languageService.getSelectedLanguage();
+  }
+
+  switchLanguage(language: string) {
+    this.languageService.switchLanguage(language);
+    // Update the selected language for the button label
+    this.selectedLanguage = this.languageService.getSelectedLanguage();
+  }
 }
